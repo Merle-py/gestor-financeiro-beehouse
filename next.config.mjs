67439e674aab749c359774be692248c159fbf0da
Gabriel <gbernardo21@gmail.com>
2025-11-19 14:09:04 -0300

    fix: remover config proibida do next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Ignora verificação de erros de estilo
-    ignoreDuringBuilds: true,
-  },
-  // Se houver erro de typescript, ignora também
-  typescript: {
-    ignoreBuildErrors: true,
-  }
+    // Mantemos apenas a configuração de Typescript para garantir
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
